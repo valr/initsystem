610c6c9b0a2b3d14265a206e04ea6bc2b342b251
Valère Monseur <valere.monseur@ymail.com>
2018-08-11 00:01:50 +0200

    implement a libsystemd stub library to avoid recompiling packages

---
+++ initsystem/systemd/libsystemd.c
@@ -0,0 +1,106 @@
+
+#include <errno.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/uio.h>
+
+typedef char sd_journal;
+
+/* sd-journal */
+
+extern int sd_journal_sendv(const struct iovec *iov, int n) {
+    return 0;
+}
+
+extern int sd_journal_stream_fd(const char *identifier, int priority, int level_prefix) {
+    return -1;
+}
+
+extern int sd_journal_open(sd_journal **ret, int flags) {
+    return 0;
+}
+
+extern void sd_journal_close(sd_journal *j) {
+    return;
+}
+
+extern int sd_journal_next(sd_journal *j) {
+    return 0;
+}
+
+extern int sd_journal_previous_skip(sd_journal *j, uint64_t skip) {
+    return 0;
+}
+
+extern int sd_journal_get_realtime_usec(sd_journal *j, uint64_t *ret) {
+    *ret = 0;
+    return 0;
+}
+
+extern int sd_journal_get_data(sd_journal *j, const char *field, const void **data, size_t *l) {
+    return -ENOENT;
+}
+
+extern int sd_journal_add_match(sd_journal *j, const void *data, size_t size) {
+    return 0;
+}
+
+extern void sd_journal_flush_matches(sd_journal *j) {
+    return;
+}
+
+extern int sd_journal_seek_tail(sd_journal *j) {
+    return 0;
+}
+
+extern int sd_journal_open_directory(sd_journal **ret, const char *path, int flags) {
+    return 0;
+}
+
+/* sd-daemon */
+
+extern int sd_booted(void) {
+    return 0;
+}
+
+extern int sd_is_socket(int fd, int family, int type, int listening) {
+    return 0;
+}
+
+extern int sd_listen_fds(int unset_environment) {
+    return 0;
+}
+
+/* sd-login */
+
+extern int sd_pid_get_owner_uid(pid_t pid, uid_t *uid) {
+    return -ENODATA;
+}
+
+extern int sd_pid_get_session(pid_t pid, char **session) {
+    return -ENODATA;
+}
+
+extern int sd_session_get_seat(const char *session, char **seat) {
+    return -ENODATA;
+}
+
+extern int sd_uid_get_seats(uid_t uid, int require_active, char ***seats) {
+    return -ENODATA;
+}
+
+extern int sd_pid_get_unit(pid_t pid, char **unit) {
+    return -ENODATA;
+}
+
+extern int sd_pid_get_user_unit(pid_t pid, char **unit) {
+    return -ENODATA;
+}
+
+extern int sd_pid_get_machine_name(pid_t pid, char **name) {
+    return -ENODATA;
+}
+
+extern int sd_pid_get_slice(pid_t pid, char **slice) {
+    return -ENODATA;
+}
